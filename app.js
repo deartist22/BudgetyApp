caea39e6774989ba4d6d873d304ca54b6ffe8e3a
deartist22 <deartist22@gmail.com>
2019-04-02 18:09:09 -0500

    Verify addItemList
    test addItemList adding newItem to the expense list or income list.

--- app.js
+++ app.js
@@ -60,7 +60,9 @@ var UIController = (function() {
 		inputType: ".add__type",
 		inputDescription: ".add__description",
 		inputValue: ".add__value",
-		inputBtn: ".add__btn"
+		inputBtn: ".add__btn",
+		incomeContainer: ".income__list",
+		expensesContainer: ".expenses__list"
 	}
 
 	return {
@@ -73,19 +75,24 @@ var UIController = (function() {
 		},
 
 		addListItem: function(obj, type) {
-			var html, newHTML;
+			var html, newHTML, element;
 			//Create HTML string with placeholder text
 			if(type === 'inc') {
+				element = DOMstrings.incomeContainer;
 				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 			} else if (type === 'exp') {
+				element = DOMstrings.expensesContainer;
 				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 			}
+			
 			//Replace the placeholder text with some actual data
 			newHTML = html.replace('%id%', obj.id);
 			newHTML = newHTML.replace('%description%', obj.description);
 			newHTML = newHTML.replace('%value%', obj.value)
+			
 			//Insert the HTML into the DOM
-		}
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
+		},
 
 		getDOMStrings: function() {
 			return DOMstrings;
@@ -98,7 +105,6 @@ var UIController = (function() {
 
 
 var controller = (function(budgetCtrl,UICtrl) {
-	var input, newItem;
 
 	var setupEventListeners = function() {
 		var DOM = UICtrl.getDOMStrings();	
@@ -113,8 +119,13 @@ var controller = (function(budgetCtrl,UICtrl) {
 	}
 
 	var ctrlAddItem = function() {
-		var input = UICtrl.getInput();
-		var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		var input, newItem;
+
+		input = UICtrl.getInput();
+
+		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+		UICtrl.addListItem(newItem, input.type);
 	};
 
 	return {
