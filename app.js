53a5f55daa0e9a6cce737c722729048d8ca5c5e3
deartist22 <deartist22@gmail.com>
2019-04-01 21:36:49 -0500

    Create init function
    Add all event listeners to a method. Create init function, adding all event listeners to it, is working.

--- app.js
+++ app.js
@@ -8,15 +8,24 @@ var budgetController = (function () {
 
 var UIController = (function() {
 
-
+	var DOMstrings = {
+		inputType: ".add__type",
+		inputDescription: ".add__description",
+		inputValue: ".add__value",
+		inputBtn: ".add__btn"
+	}
 
 	return {
 		getInput: function() {
 			return {
-				type: document.querySelector(".add_type").value,
-				description: document.querySelector(".add__description").value,
-				value: document.querySelector(".add__value").value
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value
 			}
+		},
+
+		getDOMStrings: function() {
+			return DOMstrings;
 		}
 	}
 
@@ -27,17 +36,30 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl,UICtrl) {
 
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMStrings();	
+		
+		document.querySelector(DOM.inputBtn).addEventListener("click",ctrlAddItem);
+
+		document.addEventListener("keypress", function(event) {
+			if(event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		})			
+	}
+
 	var ctrlAddItem = function() {
 		var input = UICtrl.getInput();
 		console.log(input);
+	};
+
+	return {
+		init: function() {
+			setupEventListeners();
+		}
 	}
 
-	document.querySelector(".add__btn").addEventListener("click",ctrlAddItem);
-
-	document.addEventListener("keypress", function(event) {
-		if(event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
-		}
-	})
 
 })(budgetController, UIController);
+
+controller.init();
