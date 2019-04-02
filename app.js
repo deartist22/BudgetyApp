b62ba7600af3761836d6706d091164c8ca79a3fa
deartist22 <deartist22@gmail.com>
2019-04-02 17:51:03 -0500

    newHTML variable
    replace id, description and value with newHTML variable.

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var UIController = (function() {
 		},
 
 		addListItem: function(obj, type) {
-			var html;
+			var html, newHTML;
 			//Create HTML string with placeholder text
 			if(type === 'inc') {
 				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
@@ -81,7 +81,9 @@ var UIController = (function() {
 				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 			}
 			//Replace the placeholder text with some actual data
-
+			newHTML = html.replace('%id%', obj.id);
+			newHTML = newHTML.replace('%description%', obj.description);
+			newHTML = newHTML.replace('%value%', obj.value)
 			//Insert the HTML into the DOM
 		}
 
