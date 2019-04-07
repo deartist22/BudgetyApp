4f82bac1b72784016c75fe6d0ed3c98ae108761c
deartist22 <deartist22@gmail.com>
2019-04-06 20:57:45 -0500

    ctrlDeleteItem function
    ctrlDeleteItem function created & variable to select ID

--- app.js
+++ app.js
@@ -105,7 +105,8 @@ var UIController = (function() {
 		budgetLabel: ".budget__value",
 		incomeLabel: ".budget__income--value",
 		expensesLabel: ".budget__expenses--value",
-		percentageLabel: ".budget__expenses--percentage"
+		percentageLabel: ".budget__expenses--percentage",
+		container: ".container"
 	}
 
 	return {
@@ -185,7 +186,9 @@ var controller = (function(budgetCtrl,UICtrl) {
 			if(event.keyCode === 13 || event.which === 13) {
 				ctrlAddItem();
 			}
-		})			
+		})
+
+		document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);			
 	}
 
 	var updateBudget = function() {
@@ -214,6 +217,13 @@ var controller = (function(budgetCtrl,UICtrl) {
 			updateBudget();
 			
 		}
+	};
+
+	var ctrlDeleteItem = function(event) {
+		var itemID;
+
+		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
 	};
 
 	return {
