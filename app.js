11fe4f13f882b326152640715a26d70d03761c52
deartist22 <deartist22@gmail.com>
2019-04-07 20:24:26 -0500

    deleteItem function
    deleteItem function created

--- app.js
+++ app.js
@@ -58,6 +58,21 @@ var budgetController = (function () {
 			return newItem;
 		},
 
+		deleteItem: function(type, id) {
+			var ids, index;
+
+			var ids = data.allItems[type].map(function(current) {
+				return current.id;
+			})
+
+			index = ids.indexOf(id);
+
+			if(index !== -1) {
+				data.allItems[type].splice(index, 1);
+			}
+
+		},
+
 		calculateBudget: function() {
 
 			//calculate total income and expenses
@@ -229,6 +244,8 @@ var controller = (function(budgetCtrl,UICtrl) {
 			splitID = itemID.split("-");
 			type = splitID[0];
 			ID = splitID[1];
+
+			budgetCtrl.deleteItem(type, ID);
 		}
 
 	};
